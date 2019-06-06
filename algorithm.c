5e96cb0d019dafc59703bf2d42f493cd8d83d8d8
wuwu9389 <astro.usd@gmail.com>
2019-06-06 09:28:42 -0700

    final

--- algorithm.c
+++ algorithm.c
@@ -23,6 +23,25 @@ int main()
     }
 
     average = sum / n;
+    if(average >= 90 && average < 100)
+    {
+	printf("Grade: A\n");
+    }
+    if(average >= 80 && average < 90)
+    {
+        printf("Grade: B\n");
+    }
+    if(average >= 70 && average < 80)
+    {
+        printf("Grade: C\n");
+    }
+    if(average >= 60 && average < 70)
+    {
+        printf("Grade: D\n");
+    }
+    if(average < 60){
+	printf("Grade: F\n");
+    }
     printf("Average = %.2f", average);
 
     return 0;
